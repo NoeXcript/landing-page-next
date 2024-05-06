f450317d5710daf967d079d98542d0b8898512b6
Noah Magalhaes <noexcript@gmail.com>
2024-05-06 01:10:16 +0100

    feat: setup the project

---
+++ src/libs/InfoCards.ts
@@ -0,0 +1,31 @@
+import { AlarmClockOff, ArrowDownNarrowWide, ArrowUpNarrowWide, LucideIcon } from "lucide-react";
+
+interface IInfoCard {
+    title: string;
+    icon: LucideIcon;
+    bodyText: string
+    id: number
+}
+
+
+const infoCards: IInfoCard[] = [
+    {
+        title: 'Increased Sales',
+        bodyText: "Insightful's predictive analytics identify highvalue prospects for targeted pitches, boosting conversion reates and sales by up to 20%",
+        icon: ArrowUpNarrowWide,
+        id: 1
+    }, {
+        title: 'No Time Wasted',
+        bodyText: "Insightful automates personalized content creation, freeing up sales repss' time for revenue-focused activities and increased productivity.",
+        icon: AlarmClockOff,
+        id: 2
+    }, {
+        title: 'Decreased Churn',
+        bodyText: "Insightful's AI lead engagement and renewal tools reduce customer churn by up to 30% through consistent outreach and retention opportunities",
+        icon: ArrowDownNarrowWide,
+        id: 3
+    }
+]
+
+
+export default infoCards
