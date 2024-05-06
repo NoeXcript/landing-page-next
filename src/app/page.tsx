cf1f9ba873f2f49a174102a9ee15ccd7b557781c
Noah Magalhaes <noexcript@gmail.com>
2024-05-06 02:01:50 +0100

    feat: implementation and better at all projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import Spline from "@splinetool/react-spline"
+import Image from "next/image"
 import Link from "next/link"
 
 function Navbar() {
@@ -22,6 +26,30 @@ function Navbar() {
 }
 export default function Home() {
   return (
-    <main></main>
+    <main className="flex min-h-screen h-fit flex-col items-center justify-center relative">
+      <Navbar />
+      <header id="home" className="flex flex-col-reverse md:flex-row w-full h-creen max-w-7xl items-center justify-center p-8 relative overflow-x-hidden">
+        <div className="w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-bold md:text-8xl">Insightful</h1>
+            <h2 className="text-md md:text-2xl">Start growing today!</h2>
+          </div>
+          <p className="max-w-md text-sm md:text-base  text-zinc-500">Insightful is an AI-powered sales optimization tool that provies data-driven insights to boost sales performance</p>
+          <div className="w-full flex items-center justify-center md:justify-start gap-4">
+            <button className="w-48 h-12 text-sm sm:text-base rounded bg-white text-black hover:bg-fuchsia-700 hover:text-white transition-colors"> Try 7 days free!</button>
+            <button className="w-48 h-12 text-sm sm:text-base rounded hover:bg-white hover:text-white hover:bg-opacity-5 transition-color">Contact</button>
+          </div>
+          <div className="w-full h-2/4 md:h-full md:w-3/5 flex items-center justify-center relative -z-10">
+            <Spline className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[.5] items-center justify-center md:justify-start" scene='https://prod.spline.design/pvM5sSiYV2ivWraz/scene.splinecode' />
+          </div>
+        </div>
+
+      </header>
+      <section id="about" className="h-fit min-h-screen w-full flex relative items-center justify-center p-8">
+        <div className="absolute -z-10 h-full w-full overflow-hidden">
+          <Image src="/whirl.svg" />
+        </div>
+      </section>
+    </main>
   )
 }
